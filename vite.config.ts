fea73c43a6adaf205a462790189d9452ff9de542
ibalaji777 <balajiofficialmailme@gmail.com>
2026-05-02 22:51:18 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import adonisjs from '@adonisjs/vite/client'
+import inertia from '@adonisjs/inertia/vite'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    inertia({ ssr: { enabled: false, entrypoint: 'inertia/ssr.ts' } }),
+    adonisjs({ entrypoints: ['inertia/app.ts'], reload: ['resources/views/**/*.edge'] }),
+  ],
+
+  resolve: {
+    alias: {
+      '~/': `${import.meta.dirname}/inertia/`,
+      '@generated': `${import.meta.dirname}/.adonisjs/client/`,
+    },
+  },
+
+  server: {
+    hmr: {
+      port: 5174,
+    },
+    watch: {
+      ignored: ['**/storage/**', '**/tmp/**'],
+    },
+  },
+})
